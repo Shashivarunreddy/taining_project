a4cb082eb8f1513e228fbd7dad39a60979185632
Varun Reddy <2464480@cognizant.com>
2025-12-15 15:47:51 +0530

    added signup/signin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from '../components/signin/signin.component';
+import { AppComponent } from './app.component';
+import { SignupComponent } from '../components/signup/signup.component';
+import { Notfound404Component } from '../ui/notfound404/notfound404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AppComponent }, // Default route
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: Notfound404Component }, // Wildcard
+];
